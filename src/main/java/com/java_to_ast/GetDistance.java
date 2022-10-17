b46e1c955c0db145d7d540ebb13da23277f5dd75
Elijah Chou <ecool7079@gmail.com>
2022-10-17 10:23:05 -0400

    deleted unneeded method

--- src/main/java/com/java_to_ast/GetDistance.java
+++ src/main/java/com/java_to_ast/GetDistance.java
@@ -7,16 +7,17 @@ import com.opencsv.CSVWriter;
 
 public class GetDistance {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         generateDistance();
     }
 
     /**
      * This method generates the distance between each wrong code to its closest right algorithm, and write the result
      * to a csv file for further analysis.
+     *
      * @throws Exception
      */
-    public static void generateDistance() throws Exception{
+    public static void generateDistance() throws Exception {
         String wrongDir = "/Users/cynthia/Desktop/ELITE-2019/Research-autograder/Type 4";
         String rightDir = "/Users/cynthia/Desktop/ELITE-2019/Research-autograder/Type 0";
         List<String> wrongFiles = Tree2.importfiles(Paths.get(wrongDir), "");
@@ -25,10 +26,10 @@ public class GetDistance {
         double[][] score_distance = new double[wrongFiles.size()][2];
         String[] studentID = new String[wrongFiles.size()];
         int count = 0;
-        for (String file1: wrongFiles) {
+        for (String file1 : wrongFiles) {
             int min = Integer.MAX_VALUE;
             String minFile;
-            for (String file2: rightFiles) {
+            for (String file2 : rightFiles) {
                 Tree2 file_1_tree = new Tree2(file1);
                 Tree2 file_2_tree = new Tree2(file2);
                 curDis = Tree2.ZhangShasha(file_1_tree, file_2_tree);
@@ -48,51 +49,10 @@ public class GetDistance {
         CSVWriter writer = new CSVWriter(new FileWriter("/Users/cynthia/Desktop/ELITE-2019/Research-autograder/Results/data_ArrowWheel2.csv", false));
         writer.writeNext(new String[]{"Distance", "Score", "Student ID"});
         for (int i = 0; i < score_distance.length; i++) {
-            String[] tmp  = {score_distance[i][0] + "", score_distance[i][1] + "", studentID[i]};
+            String[] tmp = {score_distance[i][0] + "", score_distance[i][1] + "", studentID[i]};
             writer.writeNext(tmp);
         }
         writer.close();
 
-    }
-
-    /**
-     * This method will delete the repeated right algorithms based on distance
-     * Using this method might fasten the comparing process in the previous method
-     * @throws Exception
-     */
-    public static void deleteRepeat() throws Exception {
-        String comDir = "/Users/cynthia/Desktop/ELITE-2019/Research-autograder/Type 0";
-        List<String> filelist = Tree2.importfiles(Paths.get(comDir), "");
-        int i = 0;
-        for (String file1: filelist) {
-            int j = 0;
-            for (String file2: filelist) {
-                if (i > j || i == j) {
-                    j++;
-                    continue;
-                }
-                File f1 = new File(file1);
-                File f2 = new File(file2);
-                if((!f1.exists()) || (!f2.exists())) {
-                    j++;
-                    continue;
-                }
-                Tree2 file_1_tree = new Tree2(file1);
-                Tree2 file_2_tree = new Tree2(file2);
-                // The number 10 here is decided by several experiments. You can change it if you feel it's needed.
-                if (Tree2.ZhangShasha(file_1_tree, file_2_tree) < 10) {
-                    // delete one of the file
-                    File file = new File(file1);
-                    if(file.delete())
-                    {
-                        System.out.println("File deleted successfully " + file1);
-                    }else {
-                        System.out.println("Delete Unsuccessful");
-                    }
-                }
-                j++;
-            }
-            i++;
-        }
     }
 }
